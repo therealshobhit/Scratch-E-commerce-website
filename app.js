7af6e6cb14e545f3398fcf253ab6132c6c3f6744
therealshobhit <shobhitmathurofficial1234@gmail.com>
2026-02-17 12:30:33 +0530

    setup db

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const db = require("./")
+const db = require("./config/mongoose-connection")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
