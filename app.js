e3ee39a7b81d4e49afcdf6c3cfa387f522484f14
therealshobhit <shobhitmathurofficial1234@gmail.com>
2026-04-14 01:40:08 +0530

    c

--- app.js
+++ app.js
@@ -39,4 +39,8 @@ app.use('/owner' , ownersroutes );
 app.use('/product' , productsroutes );
 app.use('/user' , usersroutes);
 
-app.listen(3000); 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
