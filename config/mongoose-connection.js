cb9ff1ba9bfe0601e4f1fc7e1e524e7f201d97ae
therealshobhit <shobhitmathurofficial1234@gmail.com>
2026-08-12 23:06:19 +0530

    Update mongoose-connection.js
    commit

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,24 +1,47 @@
 const mongoose = require("mongoose");
-// ab na hum mongodb://127.0.0.1:27017 joye chiz h uso hta rhe h aur ek bde level pe ja rhe h jisse ..
-// const config =  require ("config"); not use as now i am rendering in mongo atlas
+const config = require("config");
 
-// this will be use now 
-const db = process.env.MONGODB_URI;
+const envMongoUri = process.env.MONGODB_URI && process.env.MONGODB_URI.trim();
+const defaultMongoUri = `${config.get("MONGODB_URI")}/scratch`;
+const mongoUri = envMongoUri || defaultMongoUri;
 
-// now we will use debug isse kya hota h ki jb hum chahe set in terminal use krke tbhi hmara output aayega
-// iske aage hum fnction bhi daaldete h usme likhte h like (development : mongoose) mtlb development ke jo bhi message ho vo show kre kuch bhi likh skte h hum
-const dbgr = require("debug")("development:mongoose");
-// we need to set our env variables to use it we will c it how to set it 
+if (!mongoUri) {
+  console.error("MongoDB URI is not configured. Set MONGODB_URI in .env or config/development.json.");
+  process.exit(1);
+}
 
-// config.get("MONGODB_URI") ISSE hoga ki jo ye h ye khud se hi decide krta h ki konsa environment mei chl rha h hmara code lije development ya kyaaa 
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log("MongoDB connected:", mongoUri);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+  });
 
-// mongoose.connect(`${config.get("MONGODB_URI")}/scratch`)// vese toh it works but some time it doesnot so we will use then and catch method we wil use that instead of console.log
-//  upr line work for local host but for public we will use
- mongoose.connect(`${db}`) 
-.then(()=>{
-    dbgr("connected")
-})
-.catch((err)=>{
-    dbgr("not connected",err)
-})
 module.exports = mongoose.connection;
+
+
+// const mongoose = require("mongoose");
+// // ab na hum mongodb://127.0.0.1:27017 joye chiz h uso hta rhe h aur ek bde level pe ja rhe h jisse ..
+// // const config =  require ("config"); not use as now i am rendering in mongo atlas
+
+// // this will be use now 
+// const db = process.env.MONGODB_URI;
+
+// // now we will use debug isse kya hota h ki jb hum chahe set in terminal use krke tbhi hmara output aayega
+// // iske aage hum fnction bhi daaldete h usme likhte h like (development : mongoose) mtlb development ke jo bhi message ho vo show kre kuch bhi likh skte h hum
+// const dbgr = require("debug")("development:mongoose");
+// // we need to set our env variables to use it we will c it how to set it 
+
+// // config.get("MONGODB_URI") ISSE hoga ki jo ye h ye khud se hi decide krta h ki konsa environment mei chl rha h hmara code lije development ya kyaaa 
+
+// // mongoose.connect(`${config.get("MONGODB_URI")}/scratch`)// vese toh it works but some time it doesnot so we will use then and catch method we wil use that instead of console.log
+// //  upr line work for local host but for public we will use
+//  mongoose.connect(`${db}`) 
+// .then(()=>{
+//     dbgr("connected")
+// })
+// .catch((err)=>{
+//     dbgr("not connected",err)
+// })
+// module.exports = mongoose.connection;
